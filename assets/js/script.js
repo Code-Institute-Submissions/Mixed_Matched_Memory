ce72f7b8fce77f237f7dd098de3ba29c04867e84
EmmaNoelles <emmas2810@gmail.com>
2021-12-18 13:02:55 +0000

    created board in java script, commented out the flipping card fuflip card function, it is not defined yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,64 @@
-console.log ("Connected!")
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options
+
+const cardArray = [
+{
+ name: 'yoda-dog',
+ img: 'assets/images/yoda-dog.png'
+},
+{
+    name: 'yoda-dog',
+    img: 'assets/images/yoda-dog.png'
+},
+{
+    name: 'spider',
+    img: 'assets/images/spider.png'
+},
+{
+    name: 'spider',
+    img: 'assets/images/spider.png'
+},
+{
+    name: 'kitten',
+    img: 'assets/images/kitten.png'
+},
+{
+name: 'kitten',
+img: 'assets/images/kitten.png'
+},
+{
+    name: 'hamster',
+    img: 'assets/images/hamster.png'
+},
+{
+    name: 'hamster',
+    img: 'assets/images/hamster.png'
+},
+{
+    name: 'curlydog',
+    img: 'assets/images/curlydog.png'
+},
+{
+    name: 'curlydog',
+    img: 'assets/images/curlydog.png'
+},
+{
+    name: 'cat',
+    img: 'assets/images/cat.png'
+},
+{
+    name: 'cat',
+    img: 'assets/images/cat.png'
+},
+]
+
+const grid = document.querySelector.('.grid')
+
+//create your board
+function createBoard() {
+    for (let i = 0, i < cardArray.length; i++){
+        var.card 
+    }
+}
+})
